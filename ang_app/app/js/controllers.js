ad6df6827209f14f33df72a592605e6311dab2e6
Tony McKendry <tonymckendry@gmail.com>
2016-03-12 22:38:27 -0700

    post layout started

--- ang_app/app/js/controllers.js
+++ ang_app/app/js/controllers.js
@@ -1,3 +1,7 @@
-app.controller("MainController", function($scope, $http, CatService){
-   // no instance variables here....
+app.controller("MainController", function($scope, $http){
+  console.log('working')
+});
+
+app.controller("postController", function($scope, $http){
+  console.log('working')
 });
